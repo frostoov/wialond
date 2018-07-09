ab79e987059d3ae21e8407cddce12cafd190ffcd
Alexander Ovechkin <ovov.yandex-team.ru>
2018-07-09 17:59:22 +0000

    Fixed gps installation

--- setup.py
+++ setup.py
@@ -3,14 +3,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='wialond',
-    version='0.6',
+    version='0.7',
     packages=find_packages(),
     install_requires=[
         'chronometer>=1.0',
         'PyYAML>=3.12',
         'gps',
     ],
-    dependency_links=[
-        'git+https://github.com/frostoov/gpsd.git#egg=gps',
-    ]
 )
